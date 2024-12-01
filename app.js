bbb2067055bd126c37443cf9ed4f6e131800d5ac
JuanJose077 <117854145+JuanJose077@users.noreply.github.com>
2024-12-01 17:41:04 -0500

    Update2 app.js

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-const routerEmpleados = require('./proyecto-express/src/routes/empleados.js'); 
+const path = require('path');
+const routerEmpleados = require(path.join(__dirname, 'src', 'routes', 'empleados.js'));
 const routerPlantas = require('../proyecto-express/src/routes/plantas.js'); 
 const routerAuth = require('./src/routes/auth.js'); // Nueva ruta de autenticación
 
